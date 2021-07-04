d3a2ab55fed54852ae5bda67ff74bd2012d4c5fe
Newbie-Alfi <aleksandr20021008@gmail.com>
2021-07-04 21:26:42 +0300

    update Separator

--- src/Separator/Separator.js
+++ src/Separator/Separator.js
@@ -1,17 +1,10 @@
-import './separator.module.css'
+import './separator.css'
 
-let separator = {
-    'wrapper' : 'separator_wrapper__3zRND',
-    'wrapper__text' : 'separator_wrapper__text__3dXCf'
-}
 let Separator = (pops) => {
     return (
-        <div className ={separator.wrapper}>
-            <h1 className={separator.wrapper__text}>{pops.text}</h1>
+        <div className ="separator">
+            <h1 className="separator__text">{pops.text}</h1>
         </div>
-        // <div className ="wrapper">
-        //     <h1 className="wrapper__text"></h1>
-        // </div>
     );
 }
 export default Separator;
