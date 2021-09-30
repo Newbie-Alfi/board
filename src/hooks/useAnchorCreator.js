23469f893cc8238e181633f9ba18e1633bc4b061
Newbie-Alfi <aleksandr20021008@gmail.com>
2021-09-30 18:29:45 +0300

    temporary changes to the students components

--- src/hooks/useAnchorCreator.js
+++ src/hooks/useAnchorCreator.js
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useContext} from 'react'
+import {useEffect, useContext} from 'react'
 import { MyContext } from '../App';
 export function useAnchorCreator(anchor){
     const value = useContext(MyContext);
